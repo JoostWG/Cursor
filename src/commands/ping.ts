8fc27dbfd3f409b6f511c7138aafe48e82cd665b
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-04-22 13:50:27 +0200

    Remove `devOnly` from ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -2,7 +2,6 @@ import BaseCommand from '../utils/baseCommand';
 import { ChatInputCommandInteraction, MessageFlags, SlashCommandBuilder } from 'discord.js';
 
 export default class PingCommand extends BaseCommand {
-    static devOnly = true;
     public static readonly data = new SlashCommandBuilder().setName('ping').setDescription('Ping!');
 
     public async execute(interaction: ChatInputCommandInteraction) {
