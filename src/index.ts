c559861692da33e861b0e4e8acd9cedee9c046b7
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-04-22 13:07:52 +0200

    Add `index.ts`

---
+++ src/index.ts
@@ -0,0 +1,91 @@
+import { discordToken } from '../config.json';
+import getCommands from './getCommands';
+import BaseCommand from './utils/baseCommand';
+import {
+    ClientOptions,
+    Collection,
+    Client as DiscordJsClient,
+    Events,
+    GatewayIntentBits,
+    MessageFlags,
+} from 'discord.js';
+
+export class Client extends DiscordJsClient {
+    private commands: Collection<string, BaseCommand>;
+
+    public constructor(options: ClientOptions) {
+        super(options);
+        this.commands = new Collection();
+
+        this.loadCommands();
+    }
+
+    private async loadCommands() {
+        for await (const command of getCommands()) {
+            try {
+                // @ts-expect-error Can't do new on abstract classes blablabla
+                const commandInstance = new command(this);
+                if (commandInstance instanceof BaseCommand) {
+                    this.commands.set(command.data.name, commandInstance);
+                }
+            } catch {
+                //
+            }
+        }
+    }
+
+    public getCommand(name: string) {
+        return this.commands.get(name);
+    }
+}
+
+const client = new Client({
+    intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds],
+});
+
+client.on('ready', async () => {
+    console.info('Ready!');
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (interaction.isChatInputCommand()) {
+        const command = client.getCommand(interaction.commandName);
+
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({
+                    content: 'There was an error while executing this command!',
+                    flags: MessageFlags.Ephemeral,
+                });
+            } else {
+                await interaction.reply({
+                    content: 'There was an error while executing this command!',
+                    flags: MessageFlags.Ephemeral,
+                });
+            }
+        }
+    } else if (interaction.isAutocomplete()) {
+        const command = client.getCommand(interaction.commandName);
+
+        if (!command || !command.autocomplete) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+        }
+
+        try {
+            await interaction.respond(await command.autocomplete(interaction));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+});
+
+client.login(discordToken);
