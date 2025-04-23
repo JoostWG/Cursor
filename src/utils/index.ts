fa68c6eb2a4ce15feaf8a041508508d233a1d41a
Ruelf <77465215+ruelf@users.noreply.github.com>
2025-04-23 11:51:19 +0200

    Refactor `getTranslations`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,13 @@
-import { Locale, LocalizationMap } from 'discord.js';
+import { LocalizationMap } from 'discord.js';
 import i18next from 'i18next';
 
 export function getTranslations(key: string): LocalizationMap {
     return Object.fromEntries(
-        // @ts-expect-error I am too lazy to properly type check this
-        i18next.options.preload.map((locale: Locale) => {
-            // @ts-expect-error I am too lazy to properly type this
-            return [locale, i18next.t(key, { lng: locale })];
-        }),
-    ) as LocalizationMap;
+        i18next.options.preload
+            ? i18next.options.preload.map((locale) => {
+                  // @ts-expect-error I am too lazy to properly type this
+                  return [locale, i18next.t(key, { lng: locale })];
+              })
+            : [],
+    );
 }
