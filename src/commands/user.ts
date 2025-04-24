9709093248d736ce1df0456e16398bb63e0d9609
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-04-24 15:41:00 +0200

    Add user command

---
+++ src/commands/user.ts
@@ -0,0 +1,34 @@
+import { BaseCommand } from '../utils/command';
+import { ChatInputCommandInteraction, EmbedBuilder, TimestampStyles, time } from 'discord.js';
+import i18next from 'i18next';
+
+export default class UserCommand extends BaseCommand {
+    public constructor() {
+        super('user', 'Returns information about a user');
+
+        this.data.addUserOption((option) => this.wrapOption(option, 'user', 'Select a user'));
+    }
+
+    public override async execute(interaction: ChatInputCommandInteraction) {
+        const user = interaction.options.getUser('user') ?? interaction.user;
+
+        const builder = new EmbedBuilder()
+            .setAuthor({
+                name: user.username,
+                iconURL: user.avatarURL({ extension: 'png', size: 1024 }) ?? undefined,
+            })
+            .addFields({
+                name: i18next.t('commands.user.createdAt', { lng: interaction.locale }),
+                value: [
+                    time(user.createdAt),
+                    time(user.createdAt, TimestampStyles.RelativeTime),
+                ].join('\n'),
+            });
+
+        if (user.accentColor) {
+            builder.setColor(user.accentColor);
+        }
+
+        await interaction.reply({ embeds: [builder] });
+    }
+}
