f518f50f64e7541804c10c22ed32b871cec3ac5c
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-05-06 11:06:57 +0200

    Remove `wrapOption` and `wrapSubcommand` methods from `BaseCommand`

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -24,35 +24,4 @@ export abstract class BaseCommand {
     public autocomplete?(
         interaction: AutocompleteInteraction,
     ): Promise<ApplicationCommandOptionChoiceData[]>;
-
-    protected wrapOption<T extends ApplicationCommandOptionBase>(
-        option: T,
-        name: string,
-        description: string,
-        subcommand?: string,
-    ) {
-        const translationKeyPrefix = subcommand
-            ? `commands:${this.name}.subcommands.${subcommand}.options.${name}`
-            : `commands:${this.name}.options.${name}.name`;
-
-        return option
-            .setName(name)
-            .setDescription(description)
-            .setNameLocalizations(getTranslations(`${translationKeyPrefix}.name`))
-            .setDescriptionLocalizations(getTranslations(`${translationKeyPrefix}.description`));
-    }
-
-    protected wrapSubcommand(
-        subcommand: SlashCommandSubcommandBuilder,
-        name: string,
-        description: string,
-    ) {
-        return subcommand
-            .setName(name)
-            .setDescription(description)
-            .setNameLocalizations(getTranslations(`commands:${this.name}.subcommands.${name}.name`))
-            .setDescriptionLocalizations(
-                getTranslations(`commands:${this.name}.subcommands.${name}.description`),
-            );
-    }
 }
