aeecdeef241a13fff70b8c63ef177e29df08b479
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-05-08 11:24:10 +0200

    Reformat ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,12 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-
-export default defineConfig([
-    globalIgnores(['dist/*']),
-    { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-    { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+export default defineConfig(
+    globalIgnores(['dist/*', 'eslint.config.mjs']),
+    { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, ...js.configs.recommended },
+    { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.browser } },
     tseslint.configs.recommended,
     {
         files: ['**/*.ts'],
@@ -30,4 +29,4 @@ export default defineConfig([
             // '@typescript-eslint/naming-convention': 'warn',
         },
     },
-]);
+);
