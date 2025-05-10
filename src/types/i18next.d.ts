4dd316af6360b61c07ba5ff5a76dbe868abbd043
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-05-10 13:55:17 +0200

    Move `i18next.d.ts` to `types` directory

--- src/types/i18next.d.ts
+++ src/types/i18next.d.ts
@@ -1,6 +1,6 @@
 import 'i18next';
-import type commands from '../locales/en-US/commands.json';
-import type common from '../locales/en-US/common.json';
+import type commands from '../../locales/en-US/commands.json';
+import type common from '../../locales/en-US/common.json';
 
 declare module 'i18next' {
     interface CustomTypeOptions {
