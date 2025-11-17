efd95ab91854f40cf76b1aac75a44ca8287c4bc2
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-11-17 22:52:05 +0100

    Fix index.ts lol

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-// import { discordToken } from '../config.json';
-// import { CursorBot } from './CursorBot';
+import { discordToken } from '../config.json';
+import { CursorBot } from './CursorBot';
 
-import { Sudoku } from './modules/sudoku';
-
-// void new CursorBot(discordToken).start();
-
-const sudoku = Sudoku.fromString(
-    '110020350390800760000000009000605090580004026000209583400508930001342608600107205',
-);
-
-console.log(sudoku.toString());
-console.log('-----------------------------');
-
-sudoku.solve();
-console.log(sudoku.toString());
+void new CursorBot(discordToken).start();
