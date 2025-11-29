8cfa4b7d270a77438a5a8c4122f35c873002dace
JoostWG <122867812+JoostWG@users.noreply.github.com>
2025-11-29 18:41:50 +0100

    Update recent results command

--- src/commands/f1/subcommands/RecentResultsSubcommand.ts
+++ src/commands/f1/subcommands/RecentResultsSubcommand.ts
@@ -1,7 +1,7 @@
-import { codeBlock } from 'discord.js';
 import type { Api } from 'jolpica-f1-api';
 import { Subcommand, type SubcommandDefinition } from '../../../lib/core';
 import type { ChatInputContext } from '../../../lib/core/context';
+import { attachment } from '../../../lib/utils';
 import { Table } from '../../../lib/utils/table';
 
 export class RecentResultsSubcommand extends Subcommand {
@@ -18,7 +18,6 @@ export class RecentResultsSubcommand extends Subcommand {
 
     protected override async handle({ interaction }: ChatInputContext): Promise<void> {
         const { data: results } = await this.api.getResults({ season: 'current', round: 'last' });
-
         const table = new Table([
             Table.row([
                 Table.cell('P', { align: 'right' }),
@@ -26,9 +25,11 @@ export class RecentResultsSubcommand extends Subcommand {
                 Table.cell('Constructor'),
                 Table.cell('Time/Status', { align: 'right' }),
                 Table.cell('Points', { align: 'right' }),
+                Table.cell('Grid', { align: 'right' }),
+                Table.cell('Diff', { align: 'right' }),
             ]),
             Table.divider(),
-            ...results.map((result) =>
+            ...results.map((result, index) =>
                 Table.row([
                     Table.cell(result.positionText, { align: 'right' }),
                     Table.cell(`${result.driver.firstName} ${result.driver.lastName}`),
@@ -37,10 +38,23 @@ export class RecentResultsSubcommand extends Subcommand {
                         align: 'right',
                     }),
                     Table.cell(result.points !== 0 ? result.points : '', { align: 'right' }),
+                    Table.cell(result.grid ?? '', { align: 'right' }),
+                    Table.cell(
+                        result.grid !== null
+                            ? this.formatPositionDiff(result.grid - index + 1)
+                            : '',
+                        { align: 'right' },
+                    ),
                 ])
             ),
         ]);
 
-        await interaction.reply({ content: codeBlock(table.render()) });
+        await interaction.reply({
+            files: [attachment(table.render(), 'results.txt')],
+        });
+    }
+
+    private formatPositionDiff(diff: number): string {
+        return `${Math.abs(diff)} ${['-', '=', '+'][Math.sign(diff) + 1]}`;
     }
 }
